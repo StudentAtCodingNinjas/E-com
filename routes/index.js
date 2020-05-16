45cf4aa7ae7e22d58c69dcc9a3ce919502165f11
StudentAtCodingNinjas <59363614+StudentAtCodingNinjas@users.noreply.github.com>
2020-05-16 15:31:38 +0530

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const productsController = require("../controller/productsController")
 
+router.use('/products',require('./products'));
+
+
+module.exports= router;
